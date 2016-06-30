aac1dd12912ef29ade0aea761adef1df1f9ad14e
Teun Kelting <teun@tjuna.com>
2016-06-30 11:09:21 +0200

    changed url in socket.io

--- src/app/factories/open-web-socket.factory.js
+++ src/app/factories/open-web-socket.factory.js
@@ -1,7 +1,7 @@
 export function webSocketFactory(socketFactory) {
   'ngInject';
 
-  var myIoSocket = io.connect('ws://api.project-p.xyz:80');
+  var myIoSocket = io.connect('wss://api.project-p.xyz:80');
   // Open a WebSocket connection
   var mySocket = socketFactory({
     ioSocket: myIoSocket
