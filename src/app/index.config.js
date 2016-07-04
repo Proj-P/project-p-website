6d07bad76bad965a3ce32ef12f659b33864d8979
Teun Kelting <teunkelting7@gmail.com>
2016-07-05 00:36:30 +0200

    added mobile styling, changed icons, removed old stuff

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -4,8 +4,11 @@ export function config ($locationProvider, toastrConfig, $httpProvider, ChartJsP
   // This is to remove the trailing # in the URL.
   $locationProvider.html5Mode(true);
 
-    ChartJsProvider.setOptions({ colours : [ '#ff7878', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'] });
-
+    ChartJsProvider.setOptions({
+      colours : [ '#ff7878', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
+      responsive: true,
+      maintainAspectRatio: true,
+    });
 
   angular.extend(toastrConfig, {
     maxOpened: 4,
